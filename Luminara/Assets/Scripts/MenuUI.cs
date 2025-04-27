edb691594e2917924e6177fffa83787e3a2904b0
Lara Cacciaguerra <141955688+huntwar@users.noreply.github.com>
2025-04-27 12:00:53 +0200

    add UI functions and create prefab

--- Luminara/Assets/Scripts/MenuUI.cs
+++ Luminara/Assets/Scripts/MenuUI.cs
@@ -1,37 +1,76 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
-    public KeyCode pickupKey = KeyCode.E; // Default pickup key
+    public TMP_InputField inputField;
+    private KeyCode pickupKey = KeyCode.E; // The active key
+    private KeyCode tempPickupKey; // Temporary key for editing
+
+    private bool isListeningForKey = false;
+
+    private void Start()
+    {
+        tempPickupKey = pickupKey;
+        inputField.text = pickupKey.ToString();
+        inputField.onSelect.AddListener(delegate { StartListeningForKey(); });
+    }
+
+    void Update()
+    {
+        if (isListeningForKey)
+        {
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    if (IsKeyboardKey(keyCode))
+                    {
+                        Debug.Log($"Keyboard key selected: {keyCode}");
+                        tempPickupKey = keyCode;
+                        inputField.text = tempPickupKey.ToString();
+                        isListeningForKey = false;
+                        EventSystem.current.SetSelectedGameObject(null); // Deselect
+                        break;
+                    }
+                    else
+                    {
+                        Debug.Log("Non-keyboard input ignored");
+                    }
+                }
+            }
+        }
+    }
+
+    private bool IsKeyboardKey(KeyCode key)
+    {
+        return key < KeyCode.Mouse0;
+    }
+
+
+    public void StartListeningForKey()
+    {
+        isListeningForKey = true;
+    }
+
+    public void SaveChanges()
+    {
+        pickupKey = tempPickupKey;
+        Debug.Log($"Saved key: {pickupKey}");
+    }
+
+    public void CancelChanges()
+    {
+        tempPickupKey = pickupKey;
+        inputField.text = pickupKey.ToString();
+        Debug.Log("Changes canceled");
+
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync("Haunted Castle");
     }
-
-    public void Settings()
-    {
-
-    }
-
-    public void PersonalizeKey()
-    {
-        foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
-        {
-            if (Input.GetKeyDown(keyCode))
-            {
-                Debug.Log($"new key set: {keyCode}");
-                pickupKey = keyCode;
-                break;
-            }
-        }
-    }
-
 }
-
-/* Volume settings
- * Pause game
- * Personalize Keys
- */
